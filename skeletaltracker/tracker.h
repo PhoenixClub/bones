2dff5b0176c8c4d8b66d3a46d15e243476fb01ca
Matthew Beckett <beckett25@gmail.com>
2012-11-14 15:20:55 +0000

    Body mapping added

--- skeletaltracker/tracker.h
+++ skeletaltracker/tracker.h
@@ -11,8 +11,9 @@ class tracker
 	    INuiSensor* m_pNuiSensor;
 
 		HRESULT startkinect();
-		void update();
+		void update();		
 		void processframe(NUI_SKELETON_FRAME*);
 		void skeletontracked(NUI_SKELETON_DATA);
 		void skeletonposition(Vector4);
+		void mapbones(const NUI_SKELETON_DATA &, NUI_SKELETON_POSITION_INDEX, NUI_SKELETON_POSITION_INDEX);
 };
