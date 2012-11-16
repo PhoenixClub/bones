00c243e6731e82db3d945fa5a5ebd8edcad0c2db
Matthew Beckett <beckett25@gmail.com>
2012-11-16 20:25:21 +0000

    removed files that are meant to be in renderer

--- skeletaltracker/main.cpp
+++ skeletaltracker/main.cpp
@@ -2,7 +2,7 @@
 int main (){
 	tracker track;
 	track.go();
-	track.noise("test.wav");
+	//track.noise("test.wav");
 	std::cin.get();
 	return 0;
 }
--- skeletaltracker/tracker.cpp
+++ skeletaltracker/tracker.cpp
@@ -42,6 +42,7 @@ HRESULT tracker::startkinect(){
             // Create an event that will be signaled when skeleton data is available
             m_hNextSkeletonEvent = CreateEventW(NULL, TRUE, FALSE, NULL);
 			std::cout<<"kinect found";
+			noise("test.wav");
             // Open a skeleton stream to receive skeleton data
             hr = m_pNuiSensor->NuiSkeletonTrackingEnable(m_hNextSkeletonEvent, 0); 
         }
@@ -50,7 +51,6 @@ HRESULT tracker::startkinect(){
     if (NULL == m_pNuiSensor || FAILED(hr))
     {
 		std::cout<<"no kinect found";
-        //SetStatusMessage(L"No ready Kinect found!");
         return E_FAIL;
     }
 
@@ -90,69 +90,7 @@ void tracker::processframe(NUI_SKELETON_FRAME* pSkeletonFrame){
         }
 	}
 }
- void tracker::skeletontracked(NUI_SKELETON_DATA skeleton){
-	  
-	  mapbones(skeleton, NUI_SKELETON_POSITION_HEAD, NUI_SKELETON_POSITION_SHOULDER_CENTER);
-      mapbones(skeleton, NUI_SKELETON_POSITION_SHOULDER_CENTER, NUI_SKELETON_POSITION_SHOULDER_LEFT);
-      mapbones(skeleton, NUI_SKELETON_POSITION_SHOULDER_CENTER, NUI_SKELETON_POSITION_SHOULDER_RIGHT);
 
-      // Render Left Arm
-      mapbones(skeleton, NUI_SKELETON_POSITION_SHOULDER_LEFT, NUI_SKELETON_POSITION_ELBOW_LEFT);
-      mapbones(skeleton, NUI_SKELETON_POSITION_ELBOW_LEFT, NUI_SKELETON_POSITION_WRIST_LEFT);
-      mapbones(skeleton, NUI_SKELETON_POSITION_WRIST_LEFT, NUI_SKELETON_POSITION_HAND_LEFT);
-
-      // Render Right Arm
-      mapbones(skeleton, NUI_SKELETON_POSITION_SHOULDER_RIGHT, NUI_SKELETON_POSITION_ELBOW_RIGHT);
-      mapbones(skeleton, NUI_SKELETON_POSITION_ELBOW_RIGHT, NUI_SKELETON_POSITION_WRIST_RIGHT);
-      mapbones(skeleton, NUI_SKELETON_POSITION_WRIST_RIGHT, NUI_SKELETON_POSITION_HAND_RIGHT);
-	 
-	  // Render Left leg
-	  mapbones(skeleton, NUI_SKELETON_POSITION_HIP_LEFT, NUI_SKELETON_POSITION_KNEE_LEFT);
-	  mapbones(skeleton, NUI_SKELETON_POSITION_KNEE_LEFT, NUI_SKELETON_POSITION_ANKLE_LEFT);
-	  mapbones(skeleton, NUI_SKELETON_POSITION_ANKLE_LEFT, NUI_SKELETON_POSITION_FOOT_LEFT);
-	  
-	  // Render Right leg
-	  mapbones(skeleton, NUI_SKELETON_POSITION_HIP_RIGHT, NUI_SKELETON_POSITION_KNEE_RIGHT);
-	  mapbones(skeleton, NUI_SKELETON_POSITION_KNEE_RIGHT, NUI_SKELETON_POSITION_ANKLE_RIGHT);
-	  mapbones(skeleton, NUI_SKELETON_POSITION_ANKLE_RIGHT, NUI_SKELETON_POSITION_FOOT_RIGHT);
-
-	  mapbones(skeleton, NUI_SKELETON_POSITION_SHOULDER_LEFT, NUI_SKELETON_POSITION_SHOULDER_CENTER);
-	  mapbones(skeleton, NUI_SKELETON_POSITION_SHOULDER_RIGHT, NUI_SKELETON_POSITION_SHOULDER_CENTER);
-	  mapbones(skeleton, NUI_SKELETON_POSITION_SHOULDER_CENTER, NUI_SKELETON_POSITION_SPINE);
-	  mapbones(skeleton, NUI_SKELETON_POSITION_SPINE, NUI_SKELETON_POSITION_HIP_CENTER);
-	  mapbones(skeleton, NUI_SKELETON_POSITION_HIP_LEFT, NUI_SKELETON_POSITION_HIP_CENTER);
-	  mapbones(skeleton, NUI_SKELETON_POSITION_HIP_RIGHT, NUI_SKELETON_POSITION_HIP_CENTER);
-	  
-}
-
- void tracker::skeletonposition(Vector4 position){
-
- }
- void tracker::mapbones(const NUI_SKELETON_DATA &skeleton, NUI_SKELETON_POSITION_INDEX jointFrom, NUI_SKELETON_POSITION_INDEX jointTo)
-      {
-        NUI_SKELETON_POSITION_TRACKING_STATE jointFromState = skeleton.eSkeletonPositionTrackingState[jointFrom];
-        NUI_SKELETON_POSITION_TRACKING_STATE jointToState = skeleton.eSkeletonPositionTrackingState[jointTo];
-
-        if (jointFromState == NUI_SKELETON_POSITION_NOT_TRACKED || jointToState == NUI_SKELETON_POSITION_NOT_TRACKED)
-        {
-          return; // nothing to draw, one of the joints is not tracked
-        }
-
-        const Vector4& jointFromPosition = skeleton.SkeletonPositions[jointFrom];
-        const Vector4& jointToPosition = skeleton.SkeletonPositions[jointTo];
-
-        // Don't draw if both points are inferred
-        if (jointFromState == NUI_SKELETON_POSITION_INFERRED || jointToState == NUI_SKELETON_POSITION_INFERRED)
-        {
-			//DrawNonTrackedBoneLine(jointFromPosition, jointToPosition); // Draw thin lines if either one of the joints is inferred
-        }
-
-        // We assume all drawn bones are inferred unless BOTH joints are tracked
-        if (jointFromState == NUI_SKELETON_POSITION_TRACKED && jointToState == NUI_SKELETON_POSITION_TRACKED)
-        {
-          //DrawTrackedBoneLine(jointFromPosition, jointToPosition); // Draw bold lines if the joints are both tracked
-        }
-      }
  
  void tracker::go(){
 		startkinect();
